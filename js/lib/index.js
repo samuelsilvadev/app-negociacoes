5d142633803d2f73e5ec254ddfb9dcb9b1c3ffdc
samuelsilva <samuelsilvawb@gmail.com>
2017-07-12 04:36:10 -0300

    Alimentnado tabela via js

--- js/lib/index.js
+++ js/lib/index.js
@@ -5,4 +5,28 @@ const campos = [
     document.querySelector('#valor'),
 ];
 
-console.log(campos);
+let tbody = document.querySelector('table tbody');
+
+const submeteForm = function submeteForm(e){
+    e.preventDefault();
+    console.log(e);
+    let newTr = document.createElement('tr');
+
+    campos.forEach((c) => {
+        let newTd = document.createElement('td');
+        newTd.textContent = c.value;
+        newTr.appendChild(newTd);
+    });
+
+    let tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    newTr.appendChild(tdVolume);
+    console.log(tbody);
+    tbody.appendChild(newTr);
+
+    e.target.reset();
+    campos[0].focus();
+
+}
+
+document.querySelector('.form').addEventListener('submit', submeteForm);
