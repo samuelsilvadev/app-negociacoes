b4e11e0fbc2bbd23a1a1af7d93af94338aed75e5
samuelsilva <samuelsilvawb@gmail.com>
2017-07-25 04:39:25 -0300

    Criando validação para mostrar ou não mensagem de sucesso

--- js/app/views/MensagemView.js
+++ js/app/views/MensagemView.js
@@ -5,7 +5,7 @@ class MensagemView{
     }
 
     _template(model){
-        return `<p class="alert alert-info">${model.texto}</p>`;
+        return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '';
     }
 
     update(model){
