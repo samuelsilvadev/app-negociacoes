79b53397dfe1ea33628ee3523d53c4432a558880
samuelsilva <samuelsilvawb@gmail.com>
2017-08-03 05:19:54 -0300

    Criando databind com view w controller

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -7,17 +7,15 @@ class NegociacaoController{
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
         
-        this._viewNegociacoes = new NegociacoesView($('#viewNegociacoes'));
         this._listaNegociacoes = new Bind (
                 new ListaNegociacoes(),
-                this._viewNegociacoes,
-                ['adiciona', 'apagarNegociacoes']);
+                new NegociacoesView($('#viewNegociacoes')),
+                'adiciona', 'apagarNegociacoes');
    
-        this._mensagemView = new MensagemView($('#mensagemView'));
         this._mensagem = new Bind (
             new Mensagem(),
-            this._mensagemView,
-            ['texto']);
+            new MensagemView($('#mensagemView')),
+            'texto');
    
     }
 
