e73d53068d90e432b814a4cf1b664229e69f4395
samuelsilva <samuelsilvawb@gmail.com>
2017-08-04 05:17:29 -0300

    Inciando outros métodos para fazer importacoes de negociacoes [NÂO FINALIZADO]

--- js/app/services/NegociacaoService.js
+++ js/app/services/NegociacaoService.js
@@ -1,9 +1,51 @@
 class NegociacaoService{
 
 
+    constructor(){
+        this._urls = {
+            semana: 'negociacoes/semana',
+            anterior: 'negociacoes/anterior',
+            retrasada: 'negociacoes/retrasada'
+        }
+    }
+
     obterNegociacaoDaSemana(callback){
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'negociacoes/semana');
+        xhr.open('GET', this._urls.semana);
+        xhr.onreadystatechange = () => {
+
+            if(xhr.readyState == 4){
+                if(xhr.status == 200){
+                    callback(null, JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))
+                }
+            }else{
+                callback('Erro ao importar Negociações', null);                
+            }
+        };
+        xhr.send();
+    }
+
+    obterNegociacaoDaSemanaAnterior(callback){
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', this._urls.anterior);
+        xhr.onreadystatechange = () => {
+
+            if(xhr.readyState == 4){
+                if(xhr.status == 200){
+                    callback(null, JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)))
+                }
+            }else{
+                callback('Erro ao importar Negociações', null);                
+            }
+        };
+        xhr.send();
+    }
+
+    obterNegociacaoDaSemanaRetrasada(callback){
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', this._urls.retrasada);
         xhr.onreadystatechange = () => {
 
             if(xhr.readyState == 4){
