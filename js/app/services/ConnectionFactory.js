dcc3db5987027342a037a638edcb180a8af2abae
samuel <samuelsilvawb@gmail.com>
2017-08-23 04:58:10 -0300

    Usando Module Pattern na função para não expor variáveis privadas

--- js/app/services/ConnectionFactory.js
+++ js/app/services/ConnectionFactory.js
@@ -1,42 +1,49 @@
-var dbName      = 'app-negociacoes';
-var versionDb   = 5;
-var stores      = [];
 
-class ConnectionFactory{
+let ConnectionFactory = (function () {
 
-    constructor(){
-        throw new Error('Classe ConnectionFactory não pode ser instanciada');
+    let dbName      = 'app-negociacoes';
+    let dbVersion   = 5;
+    let stores      = ['negociacoes'];
+    let connectionF = null;
+
+    return class ConnectionFactory{
+
+        constructor(){
+            throw new Error('Classe ConnectionFactory não pode ser instanciada');
+        }
+
+        static getConnection(){
+            return new Promise((resolve, reject) => {
+                let openDb = window.indexedDB.open(dbName, dbVersion);
+
+                openDb.onupgradeneeded = e => {
+                    console.log('Criando ou atualizando banco!');
+                    ConnectionFactory._createStores(e.target.result);               
+                };
+
+                openDb.onsuccess = e => {
+                    console.log('Conexão obtida com sucesso!');
+                    if(!connectionF)
+                        connectionF = e.target.result;
+                    resolve(connectionF);
+                };
+
+                openDb.onerror = e => {
+                    reject(e.target.error.name);
+                };
+            });
+        }
+
+        static _createStores(connection){
+            stores.forEach(store => {
+                
+                if(connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store);
+                }
+
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
     }
 
-    static getConnection(){
-        return new Promise((resolve, reject) => {
-            let openDb = window.indexedDB.open(dbName, versionDb);
-
-            openDb.onupgradeneeded = e => {
-                console.log('Criando ou atualizando banco!');
-                ConnectionFactory._createStores(e.target.result);               
-            };
-
-            openDb.onsuccess = e => {
-                console.log('Conexão obtida com sucesso!');
-                resolve(e.target.result);
-            };
-
-            openDb.onerror = e => {
-                reject(e.target.error.name);
-            };
-        });
-    }
-
-
-    static _createStores(connection){
-        stores.forEach(store => {
-            
-            if(connection.objectStoreNames.contains(store)) {
-                connection.deleteObjectStore(store);
-            }
-
-            connection.createObjectStore(store, { autoIncrement: true });
-        });
-    }
-}
+})();
