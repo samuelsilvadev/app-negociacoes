57da73d1c875bb59cc81efd594f728253a293d10
samuelsilva <samuelsilvawb@gmail.com>
2017-08-25 04:45:21 -0300

    Criando método para listar as negociacoes cadastradas no navegador

--- js/app/dao/NegociacaoDao.js
+++ js/app/dao/NegociacaoDao.js
@@ -5,13 +5,16 @@ class NegociacaoDao{
         this._store = 'negociacoes';
     }
 
+     _getObjectStore(){
+        return this._connection
+                .transaction([this._store], 'readwrite')
+                .objectStore(this._store);
+    }
+
     adiciona(negociacao){
         return new Promise((resolve, reject) =>{
 
-            let request = this._connection
-                .transaction([this._store], 'readwrite')
-                .objectStore(this._store)
-                .add(negociacao);
+            let request = this._getObjectStore().add(negociacao);
 
             request.onsuccess = e => {
                 console.log('Inserido com sucesso');                
@@ -23,5 +26,29 @@ class NegociacaoDao{
                 reject('Não foi possível criar a negociação');
             };
         })
+    }
+
+    listaTodos(){
+            
+        return new Promise((reject, resolve) => {
+
+            let cursor = this._getObjectStore().openCursor();
+            
+            let negociacoes = [];
+
+            cursor.onsuccess = e => {
+                let ponteiroAtual = e.target.result;
+
+                if(ponteiroAtual){
+                    let dado = ponteiroAtual.value;
+                    negociacoes.push(dado._data, dado._quantidade, dado._valor);
+                    ponteiroAtual.continue();
+                }else{
+                    resolve(negociacoes);
+                }
+            };
+
+            cursor.onerror = e => reject(e.target.error.name);                        
+        });
     }
 }
