4438ae17ad78bf40296284fabaedcfe75fd41412
samuel <samuelsilvawb@gmail.com>
2017-08-31 04:26:47 -0300

    Removendo pasta app do git e criando script de build no npm para babel

--- js/app/polyfill/es6.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-if (!Array.prototype.includes) {
-
-    Array.prototype.includes = function (elemento) {
-        return this.indexOf(elemento) != -1;
-    };
-}
-//# sourceMappingURL=es6.js.map
