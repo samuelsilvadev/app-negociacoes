019cf026534c6a59ee1c1ed76390c2f2c9add5c7
samuelsilva <samuelsilvawb@gmail.com>
2017-09-08 05:13:33 -0300

    Iniciando classes para cadastro de usuários

---
+++ js/app-es6/controllers/UsuarioController.js
@@ -0,0 +1,24 @@
+import {Usuario} from '../models/Usuario'
+
+export class UsuarioController{
+    
+    constructor(){
+        this._usuario = new Usuario;
+    }
+
+    adiciona(e){
+
+    }
+
+    apaga(){
+
+    }
+
+    _limpaFormulario(){
+    
+    }
+
+    _exibeUmaMensagem(mensagem){
+       
+    }
+}
---
+++ js/app-es6/models/Usuario.js
@@ -0,0 +1,34 @@
+export class Usuario{
+
+    constructor(nome, sobrenome = '', dataNascimento, email, senha){
+        this._nome = nome;
+        this._sobrenome = sobrenome;
+        this._dataNascimento = dataNascimento;
+        this._email = email;
+        this._senha = senha;
+    }
+
+    get nome(){
+        return this._nome;
+    }
+
+    get sobrenome(){
+        return this._sobrenome;
+    }
+
+    get dataNascimento(){
+        return this._dataNascimento;
+    }
+
+    get email(){
+        return this._email;
+    }
+
+    get senha(){
+        return this._senha;
+    }
+    
+    isEquals(outroUsuario){
+        return JSON.stringify(this) == JSON.stringify(outroUsuario);
+    }
+}
