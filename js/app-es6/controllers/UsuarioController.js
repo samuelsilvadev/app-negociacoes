ba7de821bb15f35b54d5d502f8d9d0afd52bb0a7
samuelsilva <samuelsilvawb@gmail.com>
2017-09-08 05:19:57 -0300

    IMplementando métodos para salvar um usuário

--- js/app-es6/controllers/UsuarioController.js
+++ js/app-es6/controllers/UsuarioController.js
@@ -1,17 +1,32 @@
 import {Usuario} from '../models/Usuario'
+import {Dom} from '../helpers/Dom'
 
-export class UsuarioController{
+export class UsuarioController extends Dom{
     
     constructor(){
-        this._usuario = new Usuario;
+        this._nome = $('#nome');
+        this._sobreNome = $('#sobrenome');
+        this._dataNascimento = $('#data-nascimento');
+        this._email = $('#email');
+        this._senha = $('#senha');
     }
 
     adiciona(e){
-
+        
     }
 
     apaga(){
 
+    }
+
+    _criaUsuario(){
+        return new Usuario(
+            this._nome.value,
+            this._sobreNome.value,
+            this._dataNascimento.value,
+            this._email.value,
+            this._senha.value
+        );
     }
 
     _limpaFormulario(){
